87b823f6bedca5b79bef619d46b0ed53171d4b10
Jovid Nurov <jovid.9@gmail.com>
2022-07-11 15:40:41 +0500

    aiohttp implemented for request

--- asicio/generator/sinc_scrape/program.py
+++ asicio/generator/sinc_scrape/program.py
@@ -1,16 +1,20 @@
+import asyncio
+from symbol import with_item
 import requests
 import bs4
+import aiohttp
 from colorama import Fore
 
 
-def get_html(episode_number: int) -> str:
+async def get_html(episode_number: int) -> str:
     print(Fore.YELLOW + f"Getting HTML for episode {episode_number}", flush=True)
 
     url = f'https://talkpython.fm/{episode_number}'
-    resp = requests.get(url)
-    resp.raise_for_status()
-
-    return resp.text
+    #resp = requests.get(url)
+    async with  aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            resp.raise_for_status()
+            return await resp.text()
 
 
 def get_title(html: str, episode_number: int) -> str:
@@ -24,17 +28,29 @@ def get_title(html: str, episode_number: int) -> str:
 
 
 def main():
-    get_title_range()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(get_title_range())
+    #get_title_range()
     print("Done.")
 
 
-def get_title_range():
+# async def get_title_range_old_version():
+#     # Please keep this range pretty small to not DDoS my site. ;)
+#     for n in range(150, 155):
+#         html = await get_html(n)
+#         title = get_title(html, n)
+#         print(Fore.WHITE + f"Title found: {title}", flush=True)
+
+async def get_title_range():
     # Please keep this range pretty small to not DDoS my site. ;)
-    for n in range(150, 155):
-        html = get_html(n)
+    tasks = []
+    for n in range(150, 155): #start them all
+        tasks.append((n,asyncio.create_task(get_html(n))))
+
+    for n, t in tasks: #process them all
+        html = await t
         title = get_title(html, n)
         print(Fore.WHITE + f"Title found: {title}", flush=True)
-
 
 if __name__ == '__main__':
     main()
